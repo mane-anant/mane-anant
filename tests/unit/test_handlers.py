21904d778ad1c8a18fe59e41f2165f7b418764cd
mane-anant <mane.anant2011@gmail.com>
2022-04-18 10:47:23 +0530

    Six Commit

--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -7,7 +7,7 @@ from users.models.Songs import Song
 @pytest.mark.parametrize((
     'audioFileType_result',
     'status_code',
-    'expected_resut'
+    'expected_result'
 ),(
     (
         1,200,
@@ -21,9 +21,8 @@ from users.models.Songs import Song
     
     )
     ))
-def test_getsongbytype(fixture_client,mocker,audioFileType_result,status_code,expected_resut):
+def test_getsongbytype(fixture_client,mocker,audioFileType_result,status_code,expected_result):
     audioFileType='song'
-   
     get_song_mock =mocker.patch.object(data_songs,'get_data')
     get_song_mock.return_value = audioFileType_result
     handler_response = fixture_client.get('/get/audioFileType')
@@ -38,8 +37,45 @@ def test_getsongbytype(fixture_client,mocker,audioFileType_result,status_code,ex
             audioFileType)
 
     assert handler_response.status_code == status_code
-    assert result_data == expected_resut
+    assert result_data == expected_result
 
+@pytest.mark.parametrize((
+    'get_id','status_code',
+    'expected_response'
+),[
+    (
+        1,204,
+        'Data deleted Successsully'
+    ),
+    (
+        2,204,
+        'Data deleted Successsully'
+    ),
+    (
+        3,204,
+        'Data deleted Successsully'
+    ),
+    (
+         14,204,
+        'Invalid INPUT'
+    )
+]
+)
+def test_delete_user_data(mocker,fixture_client,get_id,expected_response,status_code):
+    audioFileType='song'
+    delete_song_mock =mocker.patch.object(data_songs,'delete_data')
+    delete_song_mock.return_value = expected_response
+    handler_response = fixture_client.delete(f'/delete/{audioFileType}/{get_id}')
+    # assert delete_song_mock.called
+    assert delete_song_mock.called_with(audioFileType,get_id)
+    # assert handler_response.status_code == status_code
+    if handler_response.data.decode() != '':
+        result_data = handler_response.data.decode()
+        return result_data
+    # result_data = handler_response.data.decode()
+    assert handler_response.status_code == status_code
+    assert result_data == expected_response
+#####################################################################################
 @pytest.mark.parametrize((
     'payload','expected_response'
 ),[
@@ -57,27 +93,23 @@ def test_getsongbytype(fixture_client,mocker,audioFileType_result,status_code,ex
     ),
     (
         {'id': 1, 'name': 'nullwa','Duration':3,'Uploaded_time':'2022/3/30'},
-        'Data incorrect'
+        'Invalid Data'
     )
 ]
 )
 def test_add_user_data(mocker,fixture_client,payload,expected_response):
     create_song_mock =mocker.patch.object(data_songs,'add_data')
     create_song_mock.return_value = expected_response
-    payload1=json.dumps(payload)
-    
-    handler_response = fixture_client.post('/add',json=payload1)
-    result_data = json.loads(handler_response.data.decode())
+    handler_response = fixture_client.post('/add',json=payload)
+    if handler_response.data.decode() != '':
+        result_data = handler_response.data.decode()
+        return result_data
     # result_data = handler_response.data.decode()
-    # assert handler_response.status_code == 200
+    assert handler_response.status_code == 200
     if expected_response:
         # assert result_data == expected_response
         assert create_song_mock.called
         assert create_song_mock.call_args_list[0][0][0] == payload
-        # correlation id
         assert type(create_song_mock.call_args_list[0][0][1]) == str
     else:
         assert result_data == expected_response.errors
-
-
-
